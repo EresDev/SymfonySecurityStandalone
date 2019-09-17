5c71542d7c6cdb6e41d5b4c011b6ca3b842c763d
arslan <arslanafzal321@gmail.com>
2019-09-18 03:15:20 +0500

    First commit with failing test.

---
+++ tests/KernelTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests;
+
+use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class KernelTest extends WebTestCase
+{
+    public function testValidUsernamePassword() : void
+    {
+        $request = Request::create('/', 'GET', ['username' => 'foo', 'bar']);
+
+        $kernel = new Kernel();
+
+        $response = $kernel->handle($request);
+
+        $this->assertEquals('Greeting foo', $response->getContent());
+    }
+}
