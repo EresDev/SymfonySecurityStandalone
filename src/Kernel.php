363a8c95353fa59eb80d8ae6e4409d10b60529d1
arslan <arslanafzal321@gmail.com>
2019-09-21 03:01:17 +0500

    A simple and clean authentication complete.

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,47 +2,50 @@
 
 namespace App;
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestMatcher;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
-use Symfony\Component\Security\Http\Firewall;
-use Symfony\Component\Security\Http\FirewallMap;
+use Symfony\Component\Security\Core\Authentication\Provider\DaoAuthenticationProvider;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Encoder\EncoderFactory;
+use Symfony\Component\Security\Core\Encoder\PlaintextPasswordEncoder;
+use Symfony\Component\Security\Core\User\InMemoryUserProvider;
+use Symfony\Component\Security\Core\User\User;
+use Symfony\Component\Security\Core\User\UserChecker;
 
 class Kernel implements HttpKernelInterface
 {
     public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
     {
-        $tokenStorage = new TokenStorage();
-        $authenticationManager = new AuthenticationProviderManager([new SimpleAuthenticationProvider()]);
-        $simpleAuthenticationListener =
-            new SimpleAuthenticationListener($tokenStorage, $authenticationManager, 'main');
-        $firewallMap = new FirewallMap();
-        $requestMatcher = new RequestMatcher('^/');
-        $listeners = [$simpleAuthenticationListener];
-        $firewallMap->add($requestMatcher, $listeners);
 
-        $eventDispatcher = new EventDispatcher();
-        $firewall = new Firewall($firewallMap, $eventDispatcher);
-        $eventDispatcher->addSubscriber($firewall);
+        $token = new UsernamePasswordToken(
+            $request->request->get('username'),
+            $request->request->get('password'),
+            'default'
+        );
 
+        $userProvider = new InMemoryUserProvider([
+           'foo' => [
+               'password' => 'bar',
+               'roles' => ['ROLE_USER']
+           ]
+        ]);
 
-        $getResponseEvent = new GetResponseEvent($this, $request, $type);
-        $eventDispatcher->dispatch(KernelEvents::REQUEST, $getResponseEvent);
-        if ($getResponseEvent->hasResponse()) {
-            return $getResponseEvent->getResponse();
-        }
-        if (($token = $tokenStorage->getToken()) && $token->getUser()) {
-            $user = $token->getUsername();
-        } else {
-            $user = 'anonymous';
-        }
+        $encoderFactory = new EncoderFactory([
+            User::class => new PlaintextPasswordEncoder()
+        ]);
 
-        return new Response(sprintf('Greeting %s', $user));
+        $authManager = new AuthenticationProviderManager([
+            new DaoAuthenticationProvider(
+                $userProvider,
+                new UserChecker(),
+                'default',
+                $encoderFactory
+            )
+        ]);
+
+        $authToken = $authManager->authenticate($token);
+
+        return 'Greeting '. $authToken->getUsername();
     }
 }
--- tests/KernelTest.php
+++ tests/KernelTest.php
@@ -5,6 +5,7 @@ namespace Tests;
 use App\Kernel;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 
 class KernelTest extends WebTestCase
 {
@@ -16,6 +17,17 @@ class KernelTest extends WebTestCase
 
         $response = $kernel->handle($request);
 
-        $this->assertEquals('Greeting foo', $response->getContent());
+        $this->assertEquals('Greeting foo', $response);
+    }
+
+    public function testInvalidUsernamePassword() : void
+    {
+        $this->expectException(BadCredentialsException::class);
+
+        $request = Request::create('/', 'POST', ['username' => 'invalid', 'password' => 'bar']);
+
+        $kernel = new Kernel();
+
+        $response = $kernel->handle($request);
     }
 }
